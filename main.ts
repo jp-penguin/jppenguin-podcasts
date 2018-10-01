b3dc01ccb97b276af60be49096630639b1b026f1
Jared Parkinson <jared@parkinson.im>
2018-10-01 07:47:52 -0600

    downloading tests

--- main.ts
+++ main.ts
@@ -7,7 +7,6 @@ const args = process.argv.slice(1);
 serve = args.some(val => val === '--serve');
 
 function createWindow() {
-
   const electronScreen = screen;
   const size = electronScreen.getPrimaryDisplay().workAreaSize;
 
@@ -16,7 +15,8 @@ function createWindow() {
     x: 0,
     y: 0,
     width: size.width,
-    height: size.height
+    height: size.height,
+    webPreferences: { webSecurity: false }
   });
 
   if (serve) {
@@ -25,11 +25,13 @@ function createWindow() {
     });
     win.loadURL('http://localhost:4200');
   } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, 'dist/index.html'),
+        protocol: 'file:',
+        slashes: true
+      })
+    );
   }
 
   win.webContents.openDevTools();
@@ -41,11 +43,9 @@ function createWindow() {
     // when you should delete the corresponding element.
     win = null;
   });
-
 }
 
 try {
-
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
@@ -67,7 +67,6 @@ try {
       createWindow();
     }
   });
-
 } catch (e) {
   // Catch Error
   // throw e;
--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UiService {
   public navigationOpen = true;
-  constructor(private router: Router) {}
+  constructor(private router: Router, private httpClient: HttpClient) {}
 
   public navigatoinButtonClick() {
     this.navigationOpen = !this.navigationOpen;
@@ -30,5 +31,13 @@ export class UiService {
   }
   public playerButtonClick() {
     this.router.navigateByUrl('player');
+    this.httpClient
+      .get('https://skeptoid.com/podcast.xml', {
+        responseType: 'text',
+        observe: 'body'
+      })
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 }
