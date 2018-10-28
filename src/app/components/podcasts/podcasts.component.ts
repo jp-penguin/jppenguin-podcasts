ad45898f8f595e6a4587d76c16621aaa93ab8c98
Jared Parkinson <jared@parkinson.im>
2018-10-28 09:08:49 -0600

    Styling updates

--- src/app/components/podcasts/podcasts.component.ts
+++ src/app/components/podcasts/podcasts.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChildren,
+  QueryList,
+  ElementRef
+} from '@angular/core';
 import { SaveStateService } from '../../services/save-state.service';
 import { Router } from '@angular/router';
 import { UiService } from '../../services/ui.service';
+import * as lodash from 'lodash';
 
 @Component({
   selector: 'app-podcasts',
@@ -9,6 +16,8 @@ import { UiService } from '../../services/ui.service';
   styleUrls: ['./podcasts.component.scss']
 })
 export class PodcastsComponent implements OnInit {
+  @ViewChildren('dropdown')
+  dropdowns!: QueryList<ElementRef>;
   constructor(
     public saveState: SaveStateService,
     private router: Router,
@@ -16,6 +25,31 @@ export class PodcastsComponent implements OnInit {
   ) {}
 
   ngOnInit() {}
+
+  dropdownClick(x: string) {
+    const dd = this.dropdowns.toArray();
+
+    const temp = lodash.find(this.dropdowns.toArray(), a => {
+      return (
+        ((a as ElementRef).nativeElement as Element).getAttribute('d-m') === x
+      );
+    });
+    console.log((temp.nativeElement as Element).classList.add('show'));
+  }
+  overlayClick(x: string, event: Event) {
+    console.log();
+
+    if ((event.target as Element).classList.contains('dropdown')) {
+      const dd = this.dropdowns.toArray();
+
+      const temp = lodash.find(this.dropdowns.toArray(), a => {
+        return (
+          ((a as ElementRef).nativeElement as Element).getAttribute('d-m') === x
+        );
+      });
+      console.log((temp.nativeElement as Element).classList.remove('show'));
+    }
+  }
 
   podcastClick(id: string) {
     this.uiService.playerState = 'default';
